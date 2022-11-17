bda6416d49317f951420393862d156f7bac79537
ouillliam <wiwizekill@gmail.com>
2022-11-17 23:59:00 +0100

    Working account creation and username-already-exists error handling

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+import json
 import db   
 
 def create_app():
@@ -8,6 +10,25 @@ def create_app():
 app = create_app()
 mongo = db.config_db(app, "myDatabase")
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+@app.route("/api/user", methods = ['POST'])
+def api_user():
+    data = request.get_json()
+
+    user = db.create_user(mongo, data['username'], data['password'])
+
+    if user:
+        response = app.response_class(
+                response=json.dumps({'body' : user.login}),
+                status=200,
+                mimetype='application/json'
+            )
+        return response
+
+    else:
+        error = {'body' : 'username already exists'}
+        response = app.response_class(
+            response=json.dumps(error),
+            status=400,
+            mimetype='application/json'
+        )
+        return response
--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,29 +1,80 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './login.css'
+import './login.css';
+import md5 from 'md5';
 
 function Login() {
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
+
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [accountCreationSuccess, setAccountCreationSuccess] = useState([false,""])
+    const [accountCreationError, setAccountCreationError] = useState("")
+
+    const handleSignUp = async (event) => {
+      event.preventDefault();
+
+      if(username === undefined || username === null || username === "" 
+        || password=== undefined || password === null || password === "" ){
+          setAccountCreationSuccess((false, ''));
+          setAccountCreationError("fill in every field")
+          return
+      }
+
+      const responseAccountCreation = await fetch('/api/user', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'username': username, 'password': md5(password)})
+      })
+      
+      const res = await responseAccountCreation.json();
+
+      alert(res.body)
+
+      if (responseAccountCreation.ok){
+        setAccountCreationSuccess([true, res.body])
+      }
+      else{
+       setAccountCreationSuccess([false, ""])
+       setAccountCreationError(res.body)
+      }
+
+    }
+
+
     return(
     <div className='app-wrapper'>
         <div className="login-wrapper p-5 shadow">
         <h1>Log In</h1>
-        <form className='d-flex flex-column'>
+        <form className='d-flex flex-column align-items-center'>
           <label>
             <p>Username</p>
-            <input type="text" onChange={e => setUserName(e.target.value)}/>
+            <input type="text" value={username} onChange={e => setUserName(e.target.value)}/>
           </label>
           <label>
             <p>Password</p>
-            <input type="password" onChange={e => setPassword(e.target.value)}/>
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
           </label>
-          <div>
-            <button type="submit" className='mt-3 btn btn-primary'>Sign in</button>
+          <div className="align-self-start">
+            <button type="submit" className='mt-3 btn btn-primary '>Sign in</button>
           </div>
-          <div>
+          <div onClick={handleSignUp} className="align-self-start">
             <p className='mt-3 button-sign-up'>No account ? Sign up 👈🏻</p>
           </div>
+          { !accountCreationSuccess[0] && accountCreationError.length > 0 &&
+            <div>
+              <p className='mt-2 alert alert-danger'>Error : {accountCreationError}</p>
+            </div>
+          }
+
+          { accountCreationSuccess[0] && 
+            <div>
+              <p className='mt-2 alert alert-success'>Account {accountCreationSuccess[1]} successfully created</p>
+            </div>
+          } 
+
         </form>
       </div>
     </div>
